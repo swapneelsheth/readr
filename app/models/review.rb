fbdae85385e2d26f1495cfd15f73f829239a81c3
Swapneel Sheth <swapneel@cis.upenn.edu>
2015-04-06 21:06:20 -0400

    now with validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -13,4 +13,8 @@
 class Review < ActiveRecord::Base
   belongs_to :book
   attr_accessible :description, :rating, :book_id
+  
+  validates :rating, presence: true, numericality: {greater_than: 1, even:true, message: "iss no good!!!"}
+  
+  validates :book, presence: {message: "does not exist"}
 end
